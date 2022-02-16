75598862f51e7a0f80b7636dbb3d90a5e61a153a
anilkumar9622 <anilchauhan.src@gmail.com>
2022-02-16 17:30:30 +0530

    check it

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ import Dashboard from './Common/Dashboard/dashboard';
 import Protected from './Pages/private/private'
 import Student from './Common/StudentList/Student';
 import CreateStudent from './Pages/createStudent';
+import LoginPage from './Pages/LoginPage';
+import SignUpPage1 from './Pages/SignUpPage1';
+import Welcome from './welcome/welcome';
 
 export default function App() {
 
   const routes = useRoutes([
+    { path:'/', element: <Welcome/> },
+    { path:'/loginPage', element: <LoginPage/> },
+    { path:'/signUpPage1', element: <SignUpPage1/> },
     { path:'/wed', element: <Management/> },
-    { path:'/', element: <Protected page={<Dashboard />} />, children: [
+    { path:'/dashboard', element: <Protected page={<Dashboard />} />, children: [
       { path: 'student', element: <Management /> },{ path: 'createStudent', element: <CreateStudent /> }] }
   ])
 return routes;
